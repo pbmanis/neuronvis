11c3708e8595b17af0a6d9a3a11303bfbbd132f0
Paul B. Manis <pmanis@med.unc.edu>
2019-05-07 07:24:37 -0400

    Clean up after directory rename

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from .hoc_reader import HocReader
-from .hoc_viewer import HocViewer
-from .hoc_graphics import HocSurface
-from .sim_result import SimulationResult
-from .swc_to_hoc import SWC
