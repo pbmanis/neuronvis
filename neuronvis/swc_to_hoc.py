81d0503b31d02dbe770c1b14f70340aab812149d
Paul B. Manis <pmanis@med.unc.edu>
2020-02-04 17:28:56 -0500

    upudate with new swc tags, and add segmap code

--- neuronvis/swc_to_hoc.py
+++ neuronvis/swc_to_hoc.py
@@ -2,6 +2,9 @@
 import numpy as np
 from pathlib import Path
 import datetime
+import argparse
+import re
+
 
 """
 SWC File format from CNIC:
@@ -30,6 +33,69 @@ Handles Singleton "sections" in swc file by inserting the last parent segment in
 
 """
 
+# standard SWC types:
+swc_sectypes = {
+  #  0: 'undefined',
+    1: 'soma',
+    2: 'axon',
+    3: 'basal_dendrite',
+    4: 'apical_dendrite',
+    5: 'custom', # (user-defined preferences)
+    6: 'unspecified_neurites',
+    7: 'glia_processes', # who knows why this is in here… 
+    10: 'hillock',
+    11: 'unmyelinatedaxon',
+    12: 'dendrite', # 'hub',
+   # 13: 'proximal_dendrite',
+    #14: 'distal_dendrite',
+}
+
+# section types for SBEM data on bushy cells (additional definitions)
+sbem_sectypes = {
+    #new swc mapping
+    0:  'Undefined',
+    1:  'soma',
+    2:  'Myelinated_Axon',
+    3:  'Basal_Dendrite',
+    4:  'Apical_Dendrite',
+    5:  'Custom',
+    6:  'Unspecified_Neurites',
+    7:  'Glia_Processes',
+    8:  'Blank',
+    9:  'Blank',
+    10: 'Axon_Hillock',
+    11: 'Unmyelinated_Axon',
+    12: 'Dendritic_Hub',
+    13: 'Proximal_Dendrite',
+    14: 'Distal_Dendrite',
+    15: 'Axon_Initial_Segment',
+    16: 'Axon_Heminode',
+    17: 'Axon_Node',
+    18: 'Dendritic_Swelling',
+    
+}
+
+# crenaming of cell parts to match cnmodel data tables (temporary)
+renaming = {'basal_dendrite': 'dendrite',
+            'Basal_Dendrite': 'dendrite',
+            'Apical_Dendrite': 'dendrite',
+            'apical_dendrite': 'dendrite',
+            'proximal_dendrite': 'dendrite',
+            'Proximal_Dendrite': 'dendrite',
+            'distal_dendrite': 'dendrite',
+            'Distal_Dendrite': 'dendrite',
+            'Dendritic_Swelling': 'dendrite',
+            'hub': 'dendrite',
+            'Dendritic_Hub': 'dendrite',
+
+            'Axon_Hillock': 'hillock',
+            'Unmyelinated_Axon': 'unmyelinatedaxon',
+            'Axon_Initial_Segment': 'initialsegment',
+            'Axon_Heminode': 'heminode',
+            'Axon_Node': 'node',
+        }
+
+
 
 class SWC(object):
     """
@@ -42,6 +108,9 @@ class SWC(object):
     types : dict or None
         A dictionary mapping {type_id: type_name} that describes the type IDs
         in the swc data (second column).
+    secmap  str (default: 'swc')
+        Which section mapping to use. swc is the standard swc mapping,
+        sbem is an extended mapping for VCN serial blockface data.
     data : ndarray or None
         Optionally, a data array may be provided instead of an swc file. This
         is used internally.
@@ -50,7 +119,7 @@ class SWC(object):
         appropriate scaling along each of the axes.
     """
     
-    def __init__(self, filename=None, types=None, data=None, scales=None):
+    def __init__(self, filename=None, types=None, secmap='swc', data=None, scales=None):
         self._dtype = [
             ('id', int), 
             ('type', int), 
@@ -66,30 +135,14 @@ class SWC(object):
         self._children = None
         self.scales = scales
         
-        self.sectypes = {
-          #  0: 'undefined',
-            1: 'soma',
-            2: 'axon',
-            3: 'dendrite', # 'basal_dendrite',
-            4: 'dendrite', # 'apical_dendrite',
-            5: 'dendrite', #'custom', # (user-defined preferences)
-            #6: 'unspecified_neurites',
-            #7: 'glia_processes', # who knows why this is in here… 
-            10: 'hillock',
-            11: 'unmyelinatedaxon',
-            12: 'dendrite', # 'hub',
-           # 13: 'proximal_dendrite',
-            #14: 'distal_dendrite',
-        }
+        if secmap == 'swc':
+            self.sectypes = swc_sectypes
+        elif secmap == 'sbem':
+            self.sectypes = sbem_sectypes
+        else:
+            raise ValueError('SWC number map type is not recognized: %s' % secmap)
         
-        self.converts = {'basal_dendrite': 'dendrite',
-                    'apical_dendrite': 'dendrite',
-                    'hub': 'dendrite',
-                    'proximal_dendrite': 'dendrite',
-                    'distal_dendrite': 'dendrite',
-                }
-        
-        if types is not None:
+        if types is not None:  # add-on or overwrite types to dictionary
             self.sectypes.update(types)
         
         if data is not None:
@@ -107,7 +160,7 @@ class SWC(object):
         print(f"Loading: {str(filename):s}")
         self.data = np.loadtxt(filename, dtype=self._dtype)
         if self.scales is not None:
-            self.scale(x=scales['x'], y=scales['y'], z=scales['z'], r=scales['r'])
+            self.rescale(x=self.scales['x'], y=self.scales['y'], z=self.scales['z'], r=self.scales['r'])
         
 
     def copy(self):
@@ -232,12 +285,12 @@ class SWC(object):
         if self.scales is None:
             hoc.append(f"// No scaling")
         else:
-            hocappend(f"// Scaling: x: {self.scales['x']:f}, y: {self.scales['y']:f}, z: {self.scales['z']:f}, r: {self.scales['r']:f}")
+            hoc.append(f"// Scaling: x: {self.scales['x']:f}, y: {self.scales['y']:f}, z: {self.scales['z']:f}, r: {self.scales['r']:f}")
         hoc.append('')
         sectypes = self.sectypes.copy()
-        print('sectypes: ', sectypes)
+        # print('sectypes: ', sectypes)
         for t in np.unique(self.data['type']):
-            print(t)
+            # print(t)
             if t not in sectypes:
                 sectypes[t] = 'type_%d' % t
         # create section lists
@@ -273,12 +326,13 @@ class SWC(object):
             # set up geometry for this section
             hoc.append('sections[%d] {' % sec_id)
             if len(sec) == 1:
-                seg = sects[sec_ids[p]][-1] # get last segement in the parent section
-                rec = self[seg]
-                if rec['r'] < 0.05:
-                    print(f"MIN DIA ENCOUNTERED: {seg:d}, {rec['r']:f}")
-                    rec['r'] = 0.05
-                hoc.append(f"  pt3dadd({rec['x']:f}, {rec['y']:f}, {rec['z']:f}, {rec['r']*2:f})  // seg={seg:d} Singleton repair: to section[sec_ids[p]:d]")
+                if p != -1:  # if a parent exists, then make this connections
+                    seg = sects[sec_ids[p]][-1] # get last segement in the parent section
+                    rec = self[seg]
+                    if rec['r'] < 0.05:
+                        print(f"MIN DIA ENCOUNTERED: {seg:d}, {rec['r']:f}")
+                        rec['r'] = 0.05
+                    hoc.append(f"  pt3dadd({rec['x']:f}, {rec['y']:f}, {rec['z']:f}, {rec['r']*2:f})  // seg={seg:d} Singleton repair: to section[{sec_ids[p]:d}]")
             for seg in sects[sec_id]:
                 rec = self[seg]
                 if rec['r'] < 0.05:
@@ -291,6 +345,9 @@ class SWC(object):
         
         open(filename, 'w').write('\n'.join(hoc))
         print(f"Wrote {str(filename):s}")
+        
+        # now generate reverse section map for reference
+        self.make_segmap(filename)
         
     @property
     def root(self):
@@ -320,7 +377,7 @@ class SWC(object):
                 return path
             path.append(node)
 
-    def scale(self, x, y, z, r):
+    def rescale(self, x, y, z, r):
         self.data['x'] *= x
         self.data['y'] *= y
         self.data['z'] *= z
@@ -370,12 +427,95 @@ class SWC(object):
             else:
                 secstr = str(tuple(sec))
             print("%ssections[%d] type=%s parent=%d %s" % (this_indent, i, typ, p, secstr))
+   
+    def make_segmap(self, fn):
+        """
+        Create a file that helps map hoc sections back to the original swc segments
+        (from hoc_swc_sectionmap.py in vcnmodel)
 
+        This requires a "hocx" file, which has the extended information about which swc
+        segment is associated with each hoc pt3dadd call. 
+ 
+        The result is a text file that looks like:
+        hocsectionname : 1,3,5,7,9   
+        where the numbers are the swc elements.
+
+        Parameters
+        ----------
+        
+        fn : str or Path
+            filename of the hoc file to use for input
+        """
+        
+        re_section = re.compile('\s*(sections\[)([0-9]*)\]\s*{')
+        re.compile('\s*(pt3dadd\()([-+]?[0-9]*\.?[0-9]+)\,\s([-+]?[0-9]*\.?[0-9]+)\,\s([-+]?[0-9]*\.?[0-9]+)\,\s([-+]?[0-9]*\.?[0-9]+)')
+        re_section = re.compile('\s*(sections\[)([0-9]*)\]\s*{')
+        re_access = re.compile('\s*(access)\s*(sections\[)([0-9]*)\]\s*')
+        re_append = re.compile('\s*([a-z]*)(.append\(\))')
+        re_connect = re.compile('\s*(connect)\s*(sections\[)([0-9]*)\](\([0-9]*\)),\s*(sections\[)([0-9]*)\](\([0-9]*\))')
+
+        re_seg = re.compile('(seg\=)([\d]*)') # '([d+])$')
+        re_endsec = re.compile('^}')
+        dout = ''
+        in_section = False
+        secstr = ''
+        print('File: ', fn)
+        with open(fn, 'r') as fh: 
+            for cnt, line in enumerate(fh):  # read the input file line by line
+                line = line.rstrip().lstrip()
+                s = re_section.match(line)
+                if s is not None:
+                    secno = s.groups()[1]
+                    secstr = (f'section[{secno:s}]: ')
+                    in_section = True
+                    swcs = []
+                    continue
+                if in_section:
+                    if re_endsec.match(line):
+                        in_section = False
+                        for swi in swcs:
+                            secstr +=(f"{swi:s}, ")
+                        # print(secstr)
+                        dout += secstr + '\n'
+                        secstr = [] # reset
+                        continue
+                    swcindex = re_seg.search(line)
+                    if swcindex is not None:
+                        swci = swcindex.groups()[1]
+                        swcs.append(swci)
+        fout = Path(fn).with_suffix('.segmap')
+        fout.write_text(dout)
+        print('Wrote hoc->swc segmap to: ', fout)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert SWC file to HOC file for NEURON',
+                    argument_default=argparse.SUPPRESS,
+                    fromfile_prefix_chars='@')
+    parser.add_argument(dest='filename', action='store',
+                   default=None,
+                   help='Select the file to convert (no default)')
+    parser.add_argument('-r', '--radiiscale', dest='radiiscale', type=float, default=1.0,
+        help = 'Set scale factor for radii')
+    parser.add_argument('-s', '--secmap', type=str, default='swc', dest='secmap',
+                    choices=['swc', 'sbem'],
+                    help='Choose section ampping')
+
+    parser.add_argument('--remap', type=bool, default=False, dest='remap',
+                    help='Remap names to basic set used in cnmodel')
+                        
+    args = parser.parse_args()
+    fn = Path(args.filename)
+    scales = {'x': 1.0, 'y': 1.0, 'z': 1.0, 'r': 1.0}
+    if args.radiiscale != 1.0:
+        scales['r'] = args.radiiscale
+    
+    if fn.is_file():
+        s = SWC(filename=fn, secmap=args.secmap, scales=scales)
+        # s.topology()
+        s.write_hoc(Path(args.filename).with_suffix('.hocx'))
+    else:
+        print(f'File "{str(fn):s}" was not found')
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 1:
-        exit()
-    s = SWC(filename=Path(sys.argv[1]))
-    # s.topology()
-    s.write_hoc(Path(sys.argv[1]).with_suffix('.hoc'))
+    main()
