be69f73432222e5f3f4c21bb4983415704967276
Paul Manis <pmanis@med.unc.edu>
2024-06-18 16:18:27 -0400

    update required package versions

--- make_env.sh
+++ make_env.sh
@@ -1,5 +1,15 @@
 ENVNAME="nvis_venv"
-python3.8 -m venv $ENVNAME
+if [ -d $ENVNAME ]
+then
+    echo "Removing previous environment: $ENVNAME"
+    set +e
+    rsync -aR --remove-source-files $ENVNAME ~/.Trash/ || exit 1
+    set -e
+    rm -R $ENVNAME
+else
+    echo "No previous environment - ok to proceed"
+fi
+python3.11 -m venv $ENVNAME
 source $ENVNAME/bin/activate
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
